bac77b9788f79617c1f22429cbb6b95be9624e43
kaleong kong <kaleongkong@yahoo.com>
2017-12-02 21:48:39 -0800

    scrolling

--- indexPage.js
+++ indexPage.js
@@ -1,4 +1,21 @@
 window.onload = function(){
   mainNavBarLogic();
   mainInventoryContainerLogic();
+  resizeinventoryListContainer();
+  scrollDownButtonMainLogic();
+
+  window.addEventListener("resize", function(){
+    resizeinventoryListContainer();
+  });
 };
+
+function resizeinventoryListContainer() {
+  var inventoryContainers = document.getElementsByClassName('inventoryContainer');
+  var inventoryListContainer = document.getElementsByClassName('inventoryListContainer')[0];
+  inventoryListContainer.style.width = '70%';
+  var inventoryContainerWidth = inventoryContainers[0].offsetWidth + 2 * (parseInt(window.getComputedStyle(inventoryContainers[0], null).margin) + 2)
+  var inventoryListContainerWidth = inventoryListContainer.offsetWidth;
+  var expectingInventoryListContainerWidth = Math.floor(inventoryListContainerWidth/inventoryContainerWidth) * inventoryContainerWidth;
+  inventoryListContainer.style.width = expectingInventoryListContainerWidth;
+  console.log(inventoryListContainer.style.width);
+}

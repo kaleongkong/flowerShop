315325e38811bc6f248e29f4babf8dfa405ad5a4
Dominic Kong <dkong@SM-MBP15-54.att.net>
2017-12-05 22:40:21 -0800

    add check points

---
+++ contactUs/contactUs.js
@@ -0,0 +1,7 @@
+/**
+1. asterisk to red
+2. narrow name and email textarea height. Take away the resizable dot at the corner.
+3. Button change color on hover.
+4. social media icon.
+5. background on the general info section.
+*/

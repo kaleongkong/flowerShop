08b1543d0bbe5bf6a8cb100380bd57f82426c801
Dominic Kong <dkong@SM-MBP15-54.att.net>
2017-12-07 21:36:53 -0800

    update

--- contactUs/contactUs.js
+++ contactUs/contactUs.js
@@ -1,5 +1,4 @@
 /**
-3. Button change style on click.
 4. social media icon.
 5. background on the general info section.
 */
@@ -8,10 +7,18 @@ window.onload = function(){
   var buttons = document.getElementsByClassName('emailUsSendButton');
   Array.from(buttons).forEach(function(button){
     button.addEventListener('mouseenter', function(e){
-      e.target.style.backgroundColor = "#f45b41"
+      e.target.style.backgroundColor = "#f45b41";
     });
     button.addEventListener('mouseleave', function(e){
-      e.target.style.backgroundColor = "#e8554e"
+      e.target.style.backgroundColor = "#e8554e";
+    });
+    button.addEventListener('mousedown', function(e){
+      e.target.style.borderBottom = "0px";
+      e.target.style.transform= "translateY(10%)";
+    });
+    button.addEventListener('mouseup', function(e){
+      e.target.style.borderBottom = "5px solid #9a3834";
+      e.target.style.transform= "translateY(0%)";
     });
   });
 }

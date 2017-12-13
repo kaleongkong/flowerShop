6bfc3ece5e700a4c9faea958e6770eccc5b6b40a
Dominic Kong <dkong@SM-MBP15-54.att.net>
2017-12-12 22:13:04 -0800

    update

--- indexPage.js
+++ indexPage.js
@@ -14,8 +14,9 @@ function resizeinventoryListContainer() {
   var inventoryListContainer = document.getElementsByClassName('inventoryListContainer')[0];
   inventoryListContainer.style.width = '80%';
   var inventoryContainerWidth = inventoryContainers[0].offsetWidth + 2 * (parseInt(window.getComputedStyle(inventoryContainers[0], null).margin) + 2)
+  var inventoryContainerHeight = inventoryContainers[0].offsetHeight + document.getElementsByClassName('inventorySelectContainer')[0].offsetHeight + document.getElementsByClassName('infoContainer')[0].offsetHeight;
   var inventoryListContainerWidth = inventoryListContainer.offsetWidth;
   var expectingInventoryListContainerWidth = Math.floor(inventoryListContainerWidth/inventoryContainerWidth) * inventoryContainerWidth;
   inventoryListContainer.style.width = expectingInventoryListContainerWidth;
-  console.log(inventoryListContainer.style.width);
+  inventoryListContainer.style.height = Math.ceil(inventoryContainers.length / (expectingInventoryListContainerWidth / inventoryContainerWidth)) * inventoryContainerHeight;
 }
